e4212427e72b371f4a4524de0cb530e0c80e2435
TranTrungTinLV <trungtintin1989@gmail.com>
2024-08-01 02:43:55 +0700

    update git

--- lib/splashGame.dart
+++ lib/splashGame.dart
@@ -1,8 +1,11 @@
+import 'dart:async';
+
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:bao_oan/HomeGame.dart';
 
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashGame extends StatefulWidget {
   static String id = 'splash_game';
@@ -15,17 +18,23 @@ class SplashGame extends StatefulWidget {
 
 class _SplashGameState extends State<SplashGame> {
   late AudioPlayer _audioPlayer;
+  late VideoPlayerController _videoPlayerController;
+  bool _showSkipButton = false;
+  late Timer _timer;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _audioPlayer = AudioPlayer();
+    _videoPlayerController =
+        VideoPlayerController.asset('assets/horro_intro.mp4');
     _playBackgroundMusic();
   }
 
   @override
   void dispose() {
     _audioPlayer.dispose();
+    _videoPlayerController.dispose();
     super.dispose();
   }
 
@@ -35,9 +44,39 @@ class _SplashGameState extends State<SplashGame> {
 
     Duration audioDuration = (await _audioPlayer.getDuration())!;
 
-    Future.delayed(audioDuration, () {
-      Navigator.pushReplacementNamed(context, HomeGame.id);
+    Future.delayed(audioDuration, _playVideo);
+  }
+
+  void _playVideo() async {
+    await _videoPlayerController.initialize();
+    _videoPlayerController.play();
+    _videoPlayerController.setLooping(false);
+
+    _videoPlayerController.addListener(() {
+      if (!_videoPlayerController.value.isPlaying &&
+          _videoPlayerController.value.isInitialized) {
+        // Chuyển sang màn hình tiếp theo sau khi video kết thúc
+        Navigator.pushReplacementNamed(context, HomeGame.id);
+      }
     });
+
+    _timer = Timer.periodic(Duration(seconds: 1), (Timer timer) {
+      if (_videoPlayerController.value.isInitialized) {
+        final position = _videoPlayerController.value.position;
+        if (position.inSeconds >= 10 && !_showSkipButton) {
+          setState(() {
+            _showSkipButton = true;
+          });
+        }
+      }
+    });
+
+    setState(() {});
+  }
+
+  void _skipVideo() {
+    _videoPlayerController.pause();
+    Navigator.pushReplacementNamed(context, HomeGame.id);
   }
 
   @override
@@ -74,7 +113,23 @@ class _SplashGameState extends State<SplashGame> {
                 ],
               ),
             ),
-          )
+          ),
+          if (_videoPlayerController.value.isInitialized)
+            Center(
+              child: AspectRatio(
+                aspectRatio: _videoPlayerController.value.aspectRatio,
+                child: VideoPlayer(_videoPlayerController),
+              ),
+            ),
+          if (_showSkipButton)
+            Positioned(
+              bottom: 10,
+              right: 90,
+              child: ElevatedButton(
+                onPressed: _skipVideo,
+                child: Text('Bỏ qua'),
+              ),
+            )
         ],
       ),
     );
